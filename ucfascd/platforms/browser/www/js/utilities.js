cc24d117646fa7120e8db13611cb72cf258c1f3b
SColchester <scolch@gmail.com>
2019-02-27 06:42:43 -0500

    Added startup reference to end of index.html file

---
+++ ucfascd/platforms/browser/www/js/utilities.js
@@ -0,0 +1,15 @@
+var httpPortNumber;
+var httpsPortNumber;
+function getPort(){
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener("load", function () {
+		var parser = new DOMParser();
+		var doc = parser.parseFromString(xhr.responseText, "application/xml");
+		httpPortNumber= doc.getElementsByTagName("node-port-http").item(0).textContent;
+		httpsPortNumber= doc.getElementsByTagName("node-port-https").item(0).textContent;
+		alert("Port : " + httpPortNumber);
+	});
+	var configLocation = "res/port.xml";
+	xhr.open("get", configLocation, true);
+	xhr.send();
+}
