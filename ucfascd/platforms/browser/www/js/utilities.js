02a5b2a6be1270753e63d9439b584f0d947ecfec
SColchester <scolch@gmail.com>
2019-03-06 04:27:45 -0500

    final commit

--- ucfascd/platforms/browser/www/js/utilities.js
+++ ucfascd/platforms/browser/www/js/utilities.js
@@ -1,12 +1,13 @@
 var httpPortNumber;
 var httpsPortNumber;
+
 function getPort(){
 	var xhr = new XMLHttpRequest();
 	xhr.addEventListener("load", function () {
 		var parser = new DOMParser();
 		var doc = parser.parseFromString(xhr.responseText, "application/xml");
-		httpPortNumber= doc.getElementsByTagName("node-port-http").item(0).textContent;
-		httpsPortNumber= doc.getElementsByTagName("node-port-https").item(0).textContent;
+		httpPortNumber = doc.getElementsByTagName("node-port-http").item(0).textContent;
+		httpsPortNumber = doc.getElementsByTagName("node-port-https").item(0).textContent;
 		alert("Port : " + httpPortNumber);
 	});
 	var configLocation = "res/port.xml";
